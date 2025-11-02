797b6e82dee4efe8beac5ef011c4486a00c6b142
Jerdon07 <jdlitaoen@gmail.com>
2025-11-02 22:01:15 +0800

    Autoloading, Namespaces, and Models: 2:50

--- routes/web.php
+++ routes/web.php
@@ -3,53 +3,49 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+class Job {
+    public static function all(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000',
+            ], [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$10,000',
+            ], [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$40,000',
+            ]
+        ];
+    }
+}
+
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000',
-            ], [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000',
-            ], [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000',
-            ]
+    return view('jobs', 
+        [
+            'jobs' => Job::all(),
         ]
-
-    ]);
+    );
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000',
-            ], [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000',
-            ], [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000',
-            ]
-            ];
+    $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
 
-            $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-
-            return view('job', [ 'job' => $job ]);
-
+    return view('job', 
+        [ 
+            'job' => $job
+        ],
+    );
 });
+
 Route::get('/contact', function () {
     return view('contact');
 });
